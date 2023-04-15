b3b5ef1d3c1f947b513a74a5780ed3a8049abced
toothlessdev <toothless042@gmail.com>
2023-04-15 21:25:24 +0900

    Add ItemImage class in image.py

--- app/colortable.py
+++ app/colortable.py
@@ -1,39 +1,50 @@
 import numpy as np
 
+# 먼셀의 10색상환 + 무채색 3가지 (검정, 회색, 흰색)
+
 color_categories = {
-    '화이트': np.array(255,255,255),
-    '블랙': np.array(0,0,0),
-    '그레이': np.array(178,178,178),
-    '네이비': np.array(0,32,102),
-    '블루': np.array(39,47,255),
-    '아이보리': np.array(254,254,237),
-    '라이트그레이': np.array(217,217,215),
-    '베이지':np.array(241,194,118),
-    '그린':np.array(),
-    '다크그레이':np.array(),
-    '스카이블루':np.array(),
-    '브라운':np.array(),
-    '옐로우':np.array(),
-    '카키':np.array(),
-    '민트':np.array(),
-    '레드':np.array(),
-    '오렌지':np.array(),
-    '핑크':np.array(),
-    '퍼플':np.array(),
-    '다크그린':np.array(),
-    '라벤더':np.array(),
-    '올리브그린':np.array(),
-    '라이트핑크':np.array(),
-    '라이트그린':np.array(),
-    '라이트옐로우':np.array(),
-    '페일핑크':np.array(),
-    '버건디':np.array(),
-    '데님':np.array(),
-    '카멜':np.array(),
-    '딥레드':np.array(),
-    '카키베이지':np.array(),
-    '연청':np.array(),
-    '중청':np.array(),
-    '진청':np.array(),
-    '흑청':np.array()   
+    "white":np.array([255,255,255]),
+    "gray":np.array([178,178,178]),
+    "black":np.array([0,0,0]),
+
+    "red":np.array([255,0,0]),
+    "orange":np.array([251,132,2]),
+    "yellow":np.array([255,252,1]),
+    "lightgreen":np.array([64,203,119]),
+    "green":np.array([39,137,42]),
+    "lightblue":np.array([22,152,189]),
+    "blue":np.array([0,82,167]),
+    "navy":np.array([0,42,149]),
+    "purple":np.array([152,18,141]),
+    "pink":np.array([222,5,106]),
+
 }
+
+
+'''
+color_categories = {
+    '화이트': np.array([220,220,220]),
+    '블랙': np.array([50,50,50]),
+    '그레이': np.array([178,178,178]),
+    '베이지':np.array([206,179,144]),
+    '브라운':np.array([127,41,12]),
+
+    # R 계열
+    '레드':np.array([255,0,0]),
+    '오렌지':np.array([249,40,1]),
+    '옐로우':np.array([254,234,1]),
+    '핑크':np.array([248,6,161]),
+
+    # G 계열
+    '그린':np.array([54,176,0]),
+    '카키':np.array([91,90,53]),
+
+    # B 계열
+    '블루': np.array([39,47,255]),
+    '스카이블루':np.array([35,195,235]),
+    '네이비': np.array([0,32,102]),
+    '민트':np.array([50,196,171]),
+    
+    '퍼플':np.array([87,12,112]),
+}
+'''
--- app/image.py
+++ app/image.py
@@ -2,31 +2,72 @@ import requests
 import numpy as np
 import os
 from PIL import Image
+import colortable
+import math
 
-from sklearn.neighbors import KNeighborsClassifier
+def get_square_distance(color1, color2):
+    return np.sum(np.square(color1-color2))
+
+# color_categories 와 color를 비교해서 euclidian distance 가 가까운 색상의 key값 리턴
+def get_color_symbol(color):
+    color_symbol = ""
+    color_distance = math.inf
+
+    for key, item in colortable.color_categories.items():
+        distance = get_square_distance(item, color)
+        if color_distance > distance:
+            color_distance = distance
+            color_symbol = str(key)
+    return color_symbol
+    
 
 class ItemImage:
-    def __init__(self, image_url, neighbors=5):
-        self.image = Image.open(requests.get(url=image_url, stream=True).raw)
+    def __init__(self, image_url, resize_size=(100,100), url_web=True):
+        if url_web:
+            self.image = Image.open(requests.get(url=image_url, stream=True).raw).resize(resize_size)
+        else:
+            self.image = Image.open(image_url).resize(resize_size)
 
-    
-    # image_pixel 에 모든 pixel 에 대해 KNN 알고리즘으로 5개로 군집화
+        self.image_pixel = self.image.convert('RGB')
+
+        self.colors = dict()
+        self.principle_color = dict()
+
+        for key, item in colortable.color_categories.items():
+            self.colors[key] = 0
+
+    def get_resized_image(self):
+        self.image.save("resized.jpg")
+
     def get_colors(self):
-        pass
+        for row in range(self.image_pixel.width):
+            for col in range(self.image_pixel.height):
+                color = np.array(self.image_pixel.getpixel((row,col)))
+                self.colors[get_color_symbol(color=color)] += 1
+        self.colors = sorted(self.colors.items(), key = lambda item: item[1], reverse=True)
 
+        return self.colors
 
-    # 
-    def get_principle_color(self):
-        pass
+    # 주 색상 n개 리턴, limit 비율 보다 적은 색상은 버림 (기본값 0.05)
+    def get_principle_color(self, n=4, limit=0.05):
+        for key, value in self.colors:
+            if value > self.image.width * self.image.height * limit:
+                self.principle_color[key] = value
+        return self.principle_color
 
 
 
 if __name__ == "__main__":
+    # test code for image.py
+    # img = ItemImage("https://image.msscdn.net/images/goods_img/20230210/3074892/3074892_16760091686503_500.jpg", url_web=True)
+    img = ItemImage("https://image.msscdn.net/images/goods_img/20210808/2052494/2052494_1_500.jpg")
+    colors= img.get_colors()
 
-    with open("testImage1.jpeg", "rb") as f:
-        image = Image.open(f)
-        image_pixel = np.asarray(image)
-        image_width, image_height = image.size
+    for color in colors:
+        print(color)
 
-        print(image_width, image_height)
-        print(image_pixel.shape)
+    principle_colors = img.get_principle_color()
+    for c in principle_colors:
+        print(c)
+
+    img.get_resized_image()
