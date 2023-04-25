52403e9ecc364837472e53c317d61015b6d4f121
toothlessdev <toothless042@gmail.com>
2023-04-25 23:34:03 +0900

    Element not found Exception Handling

--- app/app.py
+++ app/app.py
@@ -1,7 +1,14 @@
 from style import *
 from bs4 import BeautifulSoup
 
-style_lists = get_style_lists()
+style_lists = get_style_lists(page_index=1)
+
+for style_list in style_lists[0:10]:
+    print(f"codimap/views/{style_list}")
+    style_info = get_style_info(style_list)
+    for item in style_info:
+        print(get_item_info(item))
+    print()
 
 print(style_lists)
 print(len(style_lists))
